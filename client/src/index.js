81eff5547c05f99f974dd25f64cc97db4c833910
sin9270 <sin9270@gmail.com>
2020-02-23 12:19:57 +0900

    [add] #7 PWAとして動作するように、設定ファイル及びアイコン画像を追加 (#9)

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(reg => {
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    })
+    .catch(error => {
+      console.log('Registration failed with ' + error);
+    });
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
