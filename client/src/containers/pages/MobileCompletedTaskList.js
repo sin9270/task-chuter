a5c650766940e19eb96a76a29f93545eeadd820b
dragogonone <dragogoneone@gmail.com>
2020-04-25 20:02:41 +0900

    isRequired を外す

--- client/src/containers/pages/MobileCompletedTaskList.js
+++ client/src/containers/pages/MobileCompletedTaskList.js
@@ -13,9 +13,9 @@ import Header from '../organisms/header';
 
 const propTypes = {
   tasks: PropTypes.array.isRequired,
-  titleContains: PropTypes.string.isRequired,
-  noteContains: PropTypes.string.isRequired,
-  lastLog: PropTypes.object.isRequired,
+  titleContains: PropTypes.string,
+  noteContains: PropTypes.string,
+  lastLog: PropTypes.object,
   updateState: PropTypes.func.isRequired,
   load: PropTypes.func.isRequired,
   undo: PropTypes.func.isRequired,
