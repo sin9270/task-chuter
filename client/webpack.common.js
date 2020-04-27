f2279a2a575562fda87580723e4c70239d4347d2
sin9270 <sin9270@gmail.com>
2020-04-27 19:33:35 +0900

    [add] #52 bundleしたJSファイル名が毎回変わるようにする (#53)
    * 必要なmoduleをinstall
    
    * bundleしたJSファイル名が毎回変わるようにする
    
    * linterのエラーを抑えるために設定ファイルを変更

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['index.html', 'bundle.*.js'],
+    }),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+  ],
   output: {
-    filename: 'bundle.js',
     path: path.join(__dirname, 'public/'),
+    filename: 'bundle.[contenthash].js',
   },
   module: {
     rules: [
