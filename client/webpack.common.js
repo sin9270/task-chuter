ddc7f3a631c88bf41877bbdea8b8ef6bd15d0edc
sin9270 <sin9270@gmail.com>
2020-06-20 14:51:21 +0900

    [add] #2 add configs for TypeScript (#70)

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -31,6 +31,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.tsx$/,
+        use: 'ts-loader',
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
   },
 };
