187623759373d612f4f05da72aba13dc341d7442
WATANABE Shin <sin9270@gmail.com>
2020-06-20 16:41:20 +0900

    testファイルをTypeScript化

--- client/test/Button.test.tsx
+++ client/test/Button.test.tsx
@@ -1,20 +1,20 @@
 import { shallow } from 'enzyme';
-import React from 'react';
+import * as React from 'react';
 
 import Button from '../src/components/atoms/Button';
 
-describe('Test Button', () => {
+describe('Test Button', (): void => {
   const testProps = {
     children: 'Test Button',
     onClick: jest.fn(),
   };
 
-  it('子要素の文字列が表示される', () => {
+  it('子要素の文字列が表示される', (): void => {
     const wrapper = shallow(<Button {...testProps} />);
     expect(wrapper.text()).toBe(testProps.children);
   });
 
-  it('ボタンをクリックするとclickイベントが発火する', () => {
+  it('ボタンをクリックするとclickイベントが発火する', (): void => {
     const wrapper = shallow(<Button {...testProps} />);
     wrapper.simulate('click');
     expect(testProps.onClick).toHaveBeenCalled();
